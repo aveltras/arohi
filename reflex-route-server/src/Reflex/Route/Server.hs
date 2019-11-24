d8e867731fd49a8ffd71e2cd2f099d4eb0a340b8
aveltras <romain.viallard@outlook.fr>
2019-11-24 09:51:49 +0100

    Figure host from the wai request

--- reflex-route-server/src/Reflex/Route/Server.hs
+++ reflex-route-server/src/Reflex/Route/Server.hs
@@ -6,9 +6,10 @@ module Reflex.Route.Server where
 import Control.Monad.Fix (MonadFix)
 import qualified Data.ByteString.Char8 as C8 (unpack)
 import Data.Maybe (fromMaybe)
-import Data.Text (Text, pack, unpack)
+import Data.Text (Text, pack)
+import Data.Text.Encoding (decodeUtf8)
 import Network.URI (URI(..), nullURI, parseURI)
-import Network.Wai (Request, rawPathInfo)
+import Network.Wai (Request, rawPathInfo, requestHeaderHost)
 import Reflex.Dom hiding (Request)
 import Reflex.Route (RouteInfo(..), RouteT, runRouteView)
 
@@ -24,18 +25,18 @@ runServerRoute
   , PerformEvent t m
   , Show r
   )
-  => Text
-  -> Request
+  => Request
   -> (r -> Text)
   -> (Text -> Maybe r)
   -> RouteT t r m ()
   -> m ()
-runServerRoute prefix request encoder decoder widget = do
+runServerRoute request encoder decoder widget = do
   onBuild <- getPostBuild
-  let currentURI = fromMaybe nullURI $ parseURI $ unpack prefix <> C8.unpack (rawPathInfo request)
+  let prefix = (<>) "//" $ fromMaybe "" $ requestHeaderHost request
+      currentURI = fromMaybe nullURI $ parseURI $ C8.unpack prefix <> C8.unpack (rawPathInfo request)
   dRoute <- holdDyn Nothing (decoder ((pack . uriPath) currentURI) <$ onBuild)
   let ri = RouteInfo
-          { _routeInfoPrefix = prefix
+          { _routeInfoPrefix = decodeUtf8 prefix
           , _routeInfoCurrent = dRoute
           , _routeInfoEncoder = encoder
           , _routeInfoDecoder = decoder
